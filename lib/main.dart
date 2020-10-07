8e864a8de156a8f439244abd2765ce2cce063383
rushiil20 <nrushiil20@gmail.com>
2020-10-08 01:46:39 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,73 @@
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutterchat_app/screens/home.dart';
-import 'package:flutterchat_app/screens/reg.dart';
-import 'package:flutterchat_app/screens/chat.dart';
-import 'package:flutterchat_app/screens/login.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  Firebase.initializeApp();
+  runApp(MyApp());
+}
 
-  runApp(
-    MaterialApp(
-      initialRoute: "home",
-      routes: {
-        "home": (context) => Myhome(),
-        "reg": (context) => MyReg(),
-        "login": (context) => Mylogin(),
-        "chat": (context) => Mychat(),
-      },
-    ),
+web() async {
+  var url = "http://10.0.2.15/cgi-bin/web.py?x=date";
+  var r = await http.get(url);
+  // var sc = r.statusCode;
+  var dat = r.body;
+  return dat;
+  // print(data);
+}
+
+/*mybody() {
+  return Column(
+    children: <Widget>[
+      RaisedButton(
+        onPressed: web,
+        child: Text('click for linux cmd'),
+      ),
+    ],
   );
+}*/
+
+class MyApp extends StatelessWidget {
+  var fsconnect = FirebaseFirestore.instance;
+
+  myget() async {
+    var d = await fsconnect.collection("students").get();
+
+    for (var i in d.docs) {
+      print(i.data());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('mobile app'),
+        ),
+        body: Column(
+          children: <Widget>[
+            RaisedButton(
+              child: Text("send data"),
+              onPressed: () {
+                fsconnect.collection("students").add(
+                      web(),
+                    );
+                print("send....");
+              },
+            ),
+            RaisedButton(
+              child: Text('get data'),
+              onPressed: () {
+                myget();
+                print("get data . . .");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
